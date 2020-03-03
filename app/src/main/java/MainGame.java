451054198fe5a3827820824a2eaf49e87a9b1927
Madhav Aggarwal <madhav.taggarwal@gmail.com>
2020-03-03 17:52:46 -0600

    - Clean up SubGame #2 - Create & Implement MainGame

---
+++ app/src/main/java/MainGame.java
@@ -0,0 +1,41 @@
+/**
+ * Represents the larger 3x3 Tic-Tac-Toe board where each square is its own Tic-Tac-Toe game
+ */
+public class MainGame {
+	private SubGame[][] subGames; // 3x3 Array of SubGames. Parallex index with trackWins
+	private char[][] trackWins; // Stores the winner of each subGame ("x" or "o") - parallel index
+
+	public MainGame() {
+		subGames = new SubGame[3][3];
+		trackWins = new char[3][3];
+	}
+
+	/**
+	 * Plays a turn and checks for winner of a subGame
+	 * @param xSubGame The x location of the subGame
+	 * @param ySubGame The y location of the subGame
+	 * @param xClick The x location of the click in the subGame
+	 * @param yClick The y location of the click in the subGame
+	 * @param c char of the player ("x" or "o")
+	 */
+	public void playSubGameTurn(int ySubGame, int xClick, int yClick, int xSubGame, char c) {
+		SubGame thisSubGame = subGames[xSubGame][ySubGame];
+		if (!thisSubGame.playTurn(xClick, yClick, c)) {
+			// Invalid move
+			System.out.println("This was an invalid move!");
+		} else {
+			// Valid move; Check winner
+			if (thisSubGame.getWinner() != ' ') {
+				trackWins[xSubGame][ySubGame] = thisSubGame.getWinner();
+			}
+		}
+	}
+
+
+	public SubGame[][] getSubGames() {
+		return subGames;
+	}
+	public char[][] getTrackWins() {
+		return trackWins;
+	}
+}
--- app/src/main/java/SubGame.java
+++ app/src/main/java/SubGame.java
@@ -1,3 +1,6 @@
+/**
+ * A standard 3x3 Tic-Tac-Toe board with check winner functionality
+ */
 public class SubGame {
     private final int BOARD_SIZE = 3;
     private char[][] arr;
@@ -14,28 +17,26 @@ public class SubGame {
      */
     private void checkWinner() {
         // Horizontal Checks
-        for(int i = 0; i < BOARD_SIZE; i++) {
-            if(arr[i][0] == arr[i][1] && arr[i][0] == arr[i][2] && (arr[i][0] == 'x' || arr[i][0] == 'o')) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            if (arr[i][0] == arr[i][1] && arr[i][0] == arr[i][2] && (arr[i][0] == 'x' || arr[i][0] == 'o')) {
                 winner = arr[i][0];
                 return;
             }
 
         }
-
         // Vertical Checks
-        for(int i = 0; i < BOARD_SIZE; i++) {
-            if(arr[0][i] == arr[1][i] && arr[0][i] == arr[2][i] && (arr[0][i] == 'x' || arr[0][i] == 'o')) {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            if (arr[0][i] == arr[1][i] && arr[0][i] == arr[2][i] && (arr[0][i] == 'x' || arr[0][i] == 'o')) {
                 winner = arr[0][i];
                 return;
             }
         }
-
         // Diagonal Checks
-        if(arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2]  && (arr[0][0] == 'x' || arr[0][0] == 'o')) {
+        if (arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2]  && (arr[0][0] == 'x' || arr[0][0] == 'o')) {
             winner = arr[0][0];
             return;
         }
-        if(arr[0][2] == arr[1][1] && arr[0][2] == arr[2][0]  && (arr[0][2] == 'x' || arr[0][2] == 'o')) {
+        if (arr[0][2] == arr[1][1] && arr[0][2] == arr[2][0]  && (arr[0][2] == 'x' || arr[0][2] == 'o')) {
             winner = arr[0][2];
         }
     }
@@ -62,6 +63,7 @@ public class SubGame {
      * @return False if invalid move (already won or already taken). True otherwise.
      */
     public boolean playTurn(int x, int y, char c) {
+        checkWinner();
         if (winner == 'x' || winner == 'o') {
             // Already a winner
             return false;
