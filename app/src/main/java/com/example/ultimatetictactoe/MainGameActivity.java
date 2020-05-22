50f6db864fff3b106958d17b73a583f3e89b436d
virnarula <virsvirus@gmail.com>
2020-05-22 00:02:35 -0500

    Main game updates buttons with x or o image when subgame is won

--- app/src/main/java/com/example/ultimatetictactoe/MainGameActivity.java
+++ app/src/main/java/com/example/ultimatetictactoe/MainGameActivity.java
@@ -4,19 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainGameActivity extends AppCompatActivity {
 
     private MainGame game;
     private TextView title;
-    private Button[][] buttons = new Button[game.BOARD_SIZE][game.BOARD_SIZE];
+    private ImageButton[][] buttons = new ImageButton[MainGame.BOARD_SIZE][MainGame.BOARD_SIZE];
     private char currPlayer = 'x';
 
     @SuppressLint("SetTextI18n")
@@ -29,12 +32,38 @@ public class MainGameActivity extends AppCompatActivity {
         title = (TextView)findViewById(R.id.title);
         title.setText(currPlayer + "'s Turn");
 
+        //Connect buttons array to Image buttons on screen
+        for (int i = 0; i < SubGame.BOARD_SIZE; i++) {
+            for (int j = 0; j < SubGame.BOARD_SIZE; j++) {
+                String buttonID = "imgBtn" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+            }
+        }
+
         // If new game, create a new game. Other populate with given intent
         game = getIntent().getExtras() == null ?
                 new MainGame() : (MainGame) getIntent().getParcelableExtra("main game");
 
         // Check if any of the boards are winners
+        for (int i = 0; i < SubGame.BOARD_SIZE; i++) {
+            for (int j = 0; j < SubGame.BOARD_SIZE; j++) {
+                char subWinner = game.getArr()[i][j].getWinner();
+                switch (subWinner) {
+                    case 'x':
+                        buttons[i][j].setImageResource(R.drawable.x);
+                        Toast toast=Toast.makeText(getApplicationContext(),"Game at " +i+j+" has been won by " + subWinner, Toast.LENGTH_LONG);
+                        toast.show();
+                        break;
 
+                    case 'o':
+                        buttons[i][j].setImageResource(R.drawable.o);
+                        Toast toast1=Toast.makeText(getApplicationContext(),"Game at " +i+j+" has been won by " + subWinner, Toast.LENGTH_LONG);
+                        toast1.show();
+                        break;
+                }
+            }
+        }
     }
 
     public void onClick(View v) {
